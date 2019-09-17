f1045f81ebab77e95d28a22f4f3e2fa29a8adff9
Kamil <kam.laskowski@gmail.com>
2019-09-17 21:55:19 +0200

    Container component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,6 +4,7 @@ import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
+import Container from '../Container/Container';
 
 class Search extends React.Component {
   static propTypes = {
@@ -13,49 +14,53 @@ class Search extends React.Component {
     changeSearchString: PropTypes.func,
     countVisible: PropTypes.number,
     countAll: PropTypes.number,
-  }
+  };
 
   static defaultProps = {
     text: settings.search.defaultText,
     icon: settings.search.icon,
-  }
+  };
 
   state = {
     value: this.props.searchString || '',
-  }
+  };
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       value: event.target.value,
     });
   }
 
-  handleOK(){
+  handleOK() {
     this.props.changeSearchString(this.state.value);
   }
 
-  componentWillReceiveProps(newProps){
-    this.setState({value: newProps.searchString});
+  componentWillReceiveProps(newProps) {
+    this.setState({ value: newProps.searchString });
   }
 
   render() {
-    const {text, icon, countVisible, countAll} = this.props;
-    const {value} = this.state;
+    const { text, icon, countVisible, countAll } = this.props;
+    const { value } = this.state;
     return (
-      <div className={styles.component}>
-        <input
-          type='text'
-          placeholder={text}
-          value={value}
-          onChange={event => this.handleChange(event)}
-        />
-        <div className={styles.buttons}>
-          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
+      <Container>
+        <div className={styles.component}>
+          <input
+            type="text"
+            placeholder={text}
+            value={value}
+            onChange={event => this.handleChange(event)}
+          />
+          <div className={styles.buttons}>
+            <Button onClick={() => this.handleOK()}>
+              <Icon name={icon} />
+            </Button>
+          </div>
+          <div>
+            {countVisible == countAll ? '' : `${countVisible} / ${countAll}`}
+          </div>
         </div>
-        <div>
-          { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
-        </div>
-      </div>
+      </Container>
     );
   }
 }
